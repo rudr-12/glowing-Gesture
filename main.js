ce58d4292efe07d43ef09ed730001e628c13d592
Rudransh <73557178+rudr-12@users.noreply.github.com>
2021-04-26 18:51:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,39 +1,44 @@
 noseX=0;
 noseY=0;
-difference=0;
-rightwristX=0;
-leftwristX=0;
-function setup(){
-video=createcapture(VIDEO);
-video.size(550,500);
-canvas.createcanvas(550,550);
-canvas.position(560,150);
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 
+  function setup() {
+  video = createCapture(VIDEO);
+  video.size(550, 500);
+  canvas = createCanvas(550, 550);
+  canvas.position(560,150);
 
-poseNet=ml5.poseNet(video,modelLoaded);
-poseNet.on('pose',gotPoses);
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
+
 function modelLoaded() {
-console.log("PoseNet Is initialized")
+  console.log('PoseNet Is Initialized!');
 }
-function gotPoses(results) {
-if (results.lenth > 0) {
-console.log(results);
-noseX= results[0].pose.nose.x;
-noseY =results[0].pose.nose.y;
-console.log("noseX ="+noseX+"noseY ="+noseY);
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("noseX = " + noseX +" noseY = " + noseY);
 
-leftwristX= resultsZ[0].pose.leftwrist.x;
-rightwristX=results[0].pose.rightwrist.x;
-difference=floor(leftwristX-rightwristX);
-        
-console.log("leftWristX="+leftwristX+"rightWristX="+rightwristX+"difference="+difference);
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+
+    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
+  }
 }
-}
+
 function draw() {
-background("969A97");
-document.getElementById("square_side").innerHTML = "Width And Height of a Square will be = " + difference +"px";
-fill("F90093");
-stroke("F90093");
-square(noseX,noseY,difference);
+background('#969A97');
+
+  document.getElementById("square_side").innerHTML = "Width And Height of a Square will be = " + difference +"px";
+  fill('#F90093');
+  stroke('#F90093');
+  square(noseX, noseY, difference);
 }
